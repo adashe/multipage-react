b67138593fe3ef14ece8c44ab140e175d06f95f1
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-02 13:22:01 -0700

    Added article cards to homepage and useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom'
+
+// page components
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { useFetch } from '../hooks/useFetch'
+
+// styles
+import '../pages/Home.css'
+
 export default function Home() {
+  const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, similique enim, impedit harum corporis adipisci tempora, velit vitae tempore nam illum iure consequuntur officia officiis nobis explicabo eligendi blanditiis minus.</p>
+    <div className="home">
+      <h2>Articles</h2>
+      { isPending && <div>Loading</div> }
+      { error && <div>{error}</div> }
+      { articles && articles.map((article) => (
+        <div key={article.id} className="card">
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
