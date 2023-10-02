ad61cfe26fe6b20ce369d7930856f622d872b9f0
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-02 11:23:28 -0700

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, similique enim, impedit harum corporis adipisci tempora, velit vitae tempore nam illum iure consequuntur officia officiis nobis explicabo eligendi blanditiis minus.</p>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, similique enim, impedit harum corporis adipisci tempora, velit vitae tempore nam illum iure consequuntur officia officiis nobis explicabo eligendi blanditiis minus.</p>
+    </div>
+  )
+}
